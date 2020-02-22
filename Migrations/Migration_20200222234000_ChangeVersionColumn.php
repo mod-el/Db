c32b8ece1bcbf519c509514bbf790a437966893c
Domenico Giambra <sviluppo@editt.it>
2020-02-23 00:37:59 +0100

    Wrong queries order

--- Migrations/Migration_20200222234000_ChangeVersionColumn.php
+++ Migrations/Migration_20200222234000_ChangeVersionColumn.php
@@ -6,12 +6,12 @@ class Migration_20200222234000_ChangeVersionColumn extends Migration
 {
 	public function exec()
 	{
-		$this->query('UPDATE model_migrations SET version = LPAD(version, 14, \'0\')');
-
 		$this->changeColumn('model_migrations', 'version', [
 			'type' => 'char(14)',
 			'null' => false,
 		]);
+
+		$this->query('UPDATE model_migrations SET version = LPAD(version, 14, \'0\')');
 
 		$this->addIndex('model_migrations', 'module_version', [
 			'module',
